c78dd57c9be432460d4f694a4a5ad655a04148d8
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-02-23 21:01:51 -0300

    Adiciona lição "Add images to the Dice Roller app"

--- Unit-1/pathway-4/DrawableDiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit-1/pathway-4/DrawableDiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,18 +18,31 @@ class MainActivity : AppCompatActivity() {
         // Acdiciona funcionalidade ao botão
         val rollButton: Button = findViewById(R.id.button2)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     /**
-     * Gera novo número aleatório e atualiza a TextView
+     * Gera novo número aleatório e atualiza a ImageView
      */
     private fun rollDice() {
         // Cria dado de 6 lados
         val dice = Dice(6)
         val diceRoll = dice.roll()
         // Atualiza o texto da TextView
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determina a imagem que será utilizada
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Atualiza a imagem e o texto descritivo
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = "Dice with number ${diceRoll} on the top face"
     }
 }
 
