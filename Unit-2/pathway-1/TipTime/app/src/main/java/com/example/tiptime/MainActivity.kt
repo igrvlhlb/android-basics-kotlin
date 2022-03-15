6e914a6fb135df18b1b88a4d1da371bfcc703471
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-15 14:22:25 -0300

    Adiciona funcionalidade ao botão do app de gorjeta

--- Unit-2/pathway-1/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit-2/pathway-1/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            binding.optionFifteenPercent.id -> 0.15
+            binding.optionEighteenPercent.id -> 0.18
+            binding.optionTwentyPercent.id -> 0.2
+            /**
+             * Não deve chegar no else!
+             * Mas se chegar, zera a gorjeta
+             */
+            else -> 0.0
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
