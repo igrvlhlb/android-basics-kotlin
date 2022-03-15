2903d86672e9175af51970a0cccbcced2987a333
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-15 16:47:17 -0300

    Adiciona classe UnitConverter
    Essa classe recebe como parâmetros um volume e uma unidade de volume.
    A partir dessas informações é possível converter o volume para outras unidades.

--- Unit-2/pathway-1/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ Unit-2/pathway-1/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -9,3 +9,74 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/**
+ * An unit converter class
+ *
+ * @param volume Original volume whose unit is specified in `unit` param.
+ * @param unit Specifies the unit being used
+ */
+class UnitConverter(private val volume: Double, unit: Units = Units.CUP) {
+    private var cups = 0.0
+
+    /**
+     * Ratios of (cups / unit)
+     */
+    private val cupsTspRatio = 1 / 48
+    private val cupsTbspRatio = 1 / 16
+    private val cupsFlOzRatio = 1 / 8
+
+    /* Converts the input volume to cups */
+    init {
+        cups = when (unit) {
+            Units.TSP -> tspToCups()
+            Units.TBSP -> tbspToCups()
+            Units.FL_OZ -> tbspToCups()
+            Units.CUP -> volume
+        }
+    }
+
+    /**
+     * Functions to convert from units to cups
+     */
+    private fun tspToCups(): Double {
+        return volume * cupsTspRatio
+    }
+
+    private fun tbspToCups(): Double {
+        return volume * cupsTbspRatio
+    }
+
+    private fun flOzToCups(): Double {
+        return volume * cupsFlOzRatio
+    }
+
+    /**
+     * Functions to convert from cups to units
+     */
+    fun toCups(): Double {
+        return cups
+    }
+
+    fun toTsp(): Double {
+        return cups / cupsTspRatio
+    }
+
+    fun toTbsp(): Double {
+        return cups / cupsTbspRatio
+    }
+
+    fun toFlOz(): Double {
+        return cups / cupsFlOzRatio
+    }
+
+    /**
+     * Units enum
+     */
+    enum class Units {
+        TSP,    // teaspoon
+        TBSP,   // tablespoon
+        FL_OZ,  // fluid ounces
+        CUP     // cups
+    }
+}
