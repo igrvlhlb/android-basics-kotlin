87f34481ec9a443efa62022dd0d69595f130d6ab
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-24 12:09:32 -0300

    Adiciona afirmações como string resources

--- Unit-2/pathway-3/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Unit-2/pathway-3/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
+import javax.sql.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
