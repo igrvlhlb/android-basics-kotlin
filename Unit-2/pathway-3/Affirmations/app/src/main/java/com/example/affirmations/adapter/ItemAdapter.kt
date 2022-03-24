3015a738ef9673fc4c4b4f84604efdf47ddb7a89
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-24 15:36:08 -0300

    Adiciona imagens ao RecyclerView

--- Unit-2/pathway-3/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Unit-2/pathway-3/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -22,17 +23,15 @@ class ItemAdapter(
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        /** [ERRADO]
-         * Esta linha de código não deve executar corretamente, mas quero ver o resultado
-         * > holder.textView.text = dataset[position].toString()
-         */
-
-        holder.textView.text = context.resources.getString(dataset[position].stringResourceId)
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int = dataset.size
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 }
