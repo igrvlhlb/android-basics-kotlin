3cf3319703d863564de81c7bff5cab2c9542727e
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-29 17:22:50 -0300

    Adiciona mais dois testes de instrumentação
    Um deles checa se os itens vizinhos estão visiveis e o outro testa
    se o outro extremo da lista está invisível (neste caso, não existe)

--- Unit-2/pathway-3/Affirmations/app/src/androidTest/java/com/example/affirmations/AffirmationsListTest.kt
+++ Unit-2/pathway-3/Affirmations/app/src/androidTest/java/com/example/affirmations/AffirmationsListTest.kt
@@ -5,11 +5,13 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -29,7 +31,33 @@ class AffirmationsListTest {
                 )
         )
 
-        onView(withText(R.string.affirmation9))
+        onView(withText(R.string.affirmation10))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun scroll_to_neighbor() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollTo<RecyclerView.ViewHolder>(
+                    hasDescendant(withText(R.string.affirmation5))
+                )
+        )
+        onView(withText(R.string.affirmation4))
+            .check(matches(isDisplayed()))
+        onView(withText(R.string.affirmation6))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun scroll_should_not_exist() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollTo<RecyclerView.ViewHolder>(
+                    hasDescendant(withText(R.string.affirmation10))
+                )
+        )
+        onView(withText(R.string.affirmation1))
+            .check(doesNotExist())
     }
 }
