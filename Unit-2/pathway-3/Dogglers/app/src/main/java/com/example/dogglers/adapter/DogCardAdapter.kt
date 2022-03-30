92b7ad3b08eea008e5125aa513e97beb5a0ef8e5
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-03-30 15:35:10 -0300

    Implementa DogCardViewHolder

--- Unit-2/pathway-3/Dogglers/app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ Unit-2/pathway-3/Dogglers/app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -18,7 +18,12 @@ package com.example.dogglers.adapter
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dogglers.R
+import com.example.dogglers.data.DataSource
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
@@ -29,16 +34,19 @@ class DogCardAdapter(
     private val layout: Int
 ): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
 
-    // TODO: Initialize the data using the List found in data/DataSource
+    val dataset = DataSource.dogs
 
     /**
      * Initialize view elements
      */
     class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
-        // TODO: Declare and initialize all of the list item UI components
+        val image: ImageView = view!!.findViewById(R.id.dog_image)
+        val name: TextView = view!!.findViewById(R.id.dog_name)
+        val age: TextView = view!!.findViewById(R.id.dog_age)
+        val hobbies: TextView = view!!.findViewById(R.id.dog_hobbies)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardAdapter.DogCardViewHolder {
         // TODO: Use a conditional to determine the layout type and set it accordingly.
         //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
         //  the vertical/horizontal list item should be used.
@@ -47,7 +55,7 @@ class DogCardAdapter(
 
         // TODO: Null should not be passed into the view holder. This should be updated to reflect
         //  the inflated layout.
-        return DogCardViewHolder(null)
+        return DogCardAdapter.DogCardViewHolder(null)
     }
 
     override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
