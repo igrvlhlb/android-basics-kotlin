f5ff353dcb7844f266bccc35a4011f0f55d6193b
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-04-07 15:27:38 -0300

    Adiciona o NavController

--- Unit-3/pathway-2/WordsApp/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ Unit-3/pathway-2/WordsApp/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -19,6 +19,15 @@ class WordListFragment : Fragment() {
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
+    private lateinit var letterId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,8 +40,6 @@ class WordListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val letterId = activity?.intent?.extras?.getString(LETTER).toString()
-
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = WordAdapter(letterId, requireContext())
