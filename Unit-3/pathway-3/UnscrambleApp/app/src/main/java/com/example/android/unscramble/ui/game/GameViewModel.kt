82ac2a13d5be8c27d23dd573ad71ca61232b0cf3
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-04-25 17:21:14 -0300

    Implementa getNextWord()

--- Unit-3/pathway-3/UnscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ Unit-3/pathway-3/UnscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,16 +6,47 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    lateinit private var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+    private val usedWordIndexes: MutableSet<Int> = mutableSetOf()
+    private lateinit var currentWord: String
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    fun getNextWord() {
+        val len = allWordsList.size
+        var nextIdx = (0..len).random()
+        while (usedWordIndexes.contains(nextIdx)) {
+            nextIdx = (0..len).random()
+        }
+        usedWordIndexes.add(nextIdx)
+        currentWord = allWordsList[nextIdx]
+        val tmpWord = currentWord.toCharArray()
+        tmpWord.shuffle()
+        while (String(tmpWord).equals(currentWord, false)) {
+            tmpWord.shuffle()
+        }
+        _currentScrambledWord = String(tmpWord)
+        currentWordCount += 1
+    }
+
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+    * Updates the next word.
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 }
