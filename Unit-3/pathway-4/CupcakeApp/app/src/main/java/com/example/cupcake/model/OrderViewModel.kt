88ec8918fa2d1805f88c5d10c072d4b100552a64
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-05-04 10:03:33 -0300

    Atribui os lifecycleOwners dos Fragments aos bindings dos layouts e calcula o valor do pedido de forma dependente do dia de retirada.

--- Unit-3/pathway-4/CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ Unit-3/pathway-4/CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -10,6 +10,7 @@ import java.util.*
 import java.text.SimpleDateFormat
 
 private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
 class OrderViewModel : ViewModel() {
     private val _quantity = MutableLiveData<Int>()
@@ -40,6 +41,7 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+        updatePrice()
     }
 
     fun hasNoFlavorSet(): Boolean {
@@ -65,6 +67,11 @@ class OrderViewModel : ViewModel() {
     }
 
     private fun updatePrice() {
-        _price.value = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        // If the user selected the first option (today) for pickup, add the surcharge
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 }
