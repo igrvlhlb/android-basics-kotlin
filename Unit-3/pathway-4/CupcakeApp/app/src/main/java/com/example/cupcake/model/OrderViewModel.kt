c75053514d121a0cad37620c2360d55b52aeb57c
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-05-11 17:22:21 -0300

    Modifica a OrderViewModel e fragment_flavor.xml para suportarem escolha de quantidade variável de sabores

--- Unit-3/pathway-4/CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ Unit-3/pathway-4/CupcakeApp/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import java.text.NumberFormat
-import java.util.*
 import java.text.SimpleDateFormat
+import java.util.*
 
 private const val PRICE_PER_CUPCAKE = 2.00
 private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
@@ -15,8 +15,9 @@ private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 class OrderViewModel : ViewModel() {
     private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> get() = _quantity
-    private val _flavor = MutableLiveData<String>()
-    val flavor: LiveData<String> get() = _flavor
+    private var _currentQuantity = 0
+    private var _quantityByFlavor = AutoGenMap(mutableMapOf())
+    val quantityByFlavor: Map<Any, LiveData<String>> get() = _quantityByFlavor
     private val _date = MutableLiveData<String>()
     val date: LiveData<String> get() = _date
     private val _price = MutableLiveData<Double>()
@@ -30,22 +31,29 @@ class OrderViewModel : ViewModel() {
         resetOrder()
     }
 
-    fun setQuantity(numberCupcakes: Int) {
-        _quantity.value = numberCupcakes
-        updatePrice()
+    private fun getFlavorQuantity(desiredFlavor: Any): Int {
+        return _quantityByFlavor[desiredFlavor]?.value?.toInt() ?: 0
     }
 
-    fun setFlavor(desiredFlavor: String) {
-        _flavor.value = desiredFlavor
+    fun incFlavor(desiredFlavor: Any) {
+        if (_currentQuantity == quantity.value) return
+        val oldQuantity = getFlavorQuantity(desiredFlavor)
+        Log.d("MainActivity", "Incremented from $oldQuantity to ${oldQuantity + 1}")
+        (_quantityByFlavor[desiredFlavor] as MutableLiveData<String>)?.value = (oldQuantity + 1).toString()
+        _currentQuantity += 1
+    }
+
+    fun decFlavor(desiredFlavor: Any) {
+        if (_currentQuantity == 0) return
+        val oldQuantity = getFlavorQuantity(desiredFlavor)
+        Log.d("MainActivity", "Decremented from $oldQuantity to ${oldQuantity - 1}")
+        (_quantityByFlavor[desiredFlavor] as MutableLiveData<String>)?.value = (oldQuantity - 1).toString()
+        _currentQuantity -= 1
     }
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
         updatePrice()
-    }
-
-    fun hasNoFlavorSet(): Boolean {
-        return _flavor.value.isNullOrEmpty()
     }
 
     private fun getPickupOptions(): List<String> {
@@ -61,9 +69,11 @@ class OrderViewModel : ViewModel() {
 
     fun resetOrder() {
         _quantity.value = 0
-        _flavor.value = ""
         _date.value = dateOptions[0]
         _price.value = 0.0
+        _currentQuantity = 0
+        _quantityByFlavor.values
+            .forEach { (it as MutableLiveData<String>).value = "0" }
     }
 
     private fun updatePrice() {
@@ -73,5 +83,18 @@ class OrderViewModel : ViewModel() {
             calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
         }
         _price.value = calculatedPrice
+    }
+
+    /** LinkedHashMap customizado que chama o método .set() quando é usado o .get() para chaves que
+     * não estão presentes.
+     * Isso faz com que todas as instâncias de objetos retornados por padrão permanecam no Map
+     */
+    class AutoGenMap(_map: MutableMap<Any,LiveData<String>>): LinkedHashMap<Any,LiveData<String>>(_map) {
+        override fun get(key: Any): LiveData<String>? {
+            if (!this.containsKey(key)) {
+                this.set(key, MutableLiveData("0"))
+            }
+            return super.get(key)
+        }
     }
 }
