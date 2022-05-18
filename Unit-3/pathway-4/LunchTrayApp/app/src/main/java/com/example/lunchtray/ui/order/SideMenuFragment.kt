ebcfda4a697a3b5288306c265e5f22c0cd98d4b7
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-05-18 17:18:56 -0300

    Define componentes de navegação e implementa os métodos

--- Unit-3/pathway-4/LunchTrayApp/app/src/main/java/com/example/lunchtray/ui/order/SideMenuFragment.kt
+++ Unit-3/pathway-4/LunchTrayApp/app/src/main/java/com/example/lunchtray/ui/order/SideMenuFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentSideMenuBinding
 import com.example.lunchtray.model.OrderViewModel
 
@@ -65,15 +67,18 @@ class SideMenuFragment : Fragment() {
      * Navigate to the accompaniments menu fragment
      */
     fun goToNextScreen() {
-        // TODO: Navigate to the AccompanimentMenuFragment
+        // Navigate to the AccompanimentMenuFragment
+        findNavController().navigate(R.id.action_sideMenuFragment_to_accompanimentMenuFragment2)
     }
 
     /**
      * Cancel the order and start over.
      */
     fun cancelOrder() {
-        // TODO: Reset order in view model
-        // TODO: Navigate back to the [StartFragment] to start over
+        // Reset order in view model
+        // Navigate back to the [StartFragment] to start over
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_sideMenuFragment_to_startOrderFragment)
     }
 
     /**
