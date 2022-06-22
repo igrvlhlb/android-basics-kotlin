b4f6084b0adf7c716bda2957f86bcbc7bcc83673
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-06-22 16:29:27 -0300

    Trata o caso em que a requisição não foi bem sucedida

--- Unit-4/MarsPhotosApp/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ Unit-4/MarsPhotosApp/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,12 +1,14 @@
 package com.example.android.marsphotos
 
 import android.provider.ContactsContract
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
@@ -25,4 +27,21 @@ fun bindRecyclerView(recyclerView: RecyclerView,
                      data: List<MarsPhoto>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> statusImageView.apply {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> statusImageView.apply {
+            visibility = View.VISIBLE
+            setImageResource(R.drawable.ic_connection_error)
+        }
+        else -> statusImageView.apply {
+            visibility = View.GONE
+        }
+    }
 }
