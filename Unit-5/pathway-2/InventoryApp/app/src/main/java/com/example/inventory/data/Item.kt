309af7a0bba57d7340184680926c8f4d685a982c
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-07-13 11:59:59 -0300

    Implementa o ListAdapter para exibir Items

--- Unit-5/pathway-2/InventoryApp/app/src/main/java/com/example/inventory/data/Item.kt
+++ Unit-5/pathway-2/InventoryApp/app/src/main/java/com/example/inventory/data/Item.kt
@@ -3,6 +3,8 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
+import java.util.*
 
 @Entity(tableName = "item")
 data class Item(
@@ -16,3 +18,6 @@ data class Item(
     @ColumnInfo(name = "quantity")
     val quantity: Int
 )
+
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(price)
