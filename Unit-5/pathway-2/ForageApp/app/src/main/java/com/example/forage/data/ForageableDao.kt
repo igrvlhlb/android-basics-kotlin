60f994813d52e08e174e40d7627e7710fea1bf11
Igor A. Vilhalba <igor.vilhalba@gmail.com>
2022-08-10 15:14:50 -0300

    Implementa Dao

--- Unit-5/pathway-2/ForageApp/app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ Unit-5/pathway-2/ForageApp/app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -15,7 +15,8 @@
  */
 package com.example.forage.data
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.forage.model.Forageable
 
 /**
  * Data Access Object for database interaction.
@@ -23,14 +24,24 @@ import androidx.room.Dao
 @Dao
 interface ForageableDao {
 
-    // TODO: implement a method to retrieve all Forageables from the database
+    // implement a method to retrieve all Forageables from the database
+    @Query("SELECT * FROM forageable_database")
+    fun getForageables()
 
-    // TODO: implement a method to retrieve a Forageable from the database by id
+    // implement a method to retrieve a Forageable from the database by id
+    @Query("SELECT * FROM forageable_database WHERE id=:id")
+    fun getForageable(id: Long)
 
-    // TODO: implement a method to insert a Forageable into the database
+    // implement a method to insert a Forageable into the database
     //  (use OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(forageable: Forageable)
 
-    // TODO: implement a method to update a Forageable that is already in the database
+    // implement a method to update a Forageable that is already in the database
+    @Update
+    fun update(forageable: Forageable)
 
-    // TODO: implement a method to delete a Forageable from the database.
+    // implement a method to delete a Forageable from the database.
+    @Delete
+    fun delete(forageable: Forageable)
 }
